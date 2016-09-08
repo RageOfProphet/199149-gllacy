9c928351fa8e3987ea33a1059b05d48338a25c45
RageOfProphet <rvlife8@gmail.com>
2016-09-08 16:12:00 +0400

    На вторую проверку

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,53 @@
 
-  var link = document.querySelector(".feedback-button");
-  var popup = document.querySelector(".modal-content");
-  var shadow = document.querySelector(".modal-shadow");
-  var close = document.querySelector(".modal-content-close");
+var link = document.querySelector(".feedback-button");
+var popup = document.querySelector(".modal-content");
+var shadow = document.querySelector(".modal-shadow");
+var close = document.querySelector(".modal-content-close");
 
-  var form = popup.querySelector("form");
-  var login = popup.querySelector("[name=login]");
-  var mail = popup.querySelector("[name=email]");
+var form = popup.querySelector("form");
+var user = popup.querySelector("[name=name]");
+var mail = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
 
-  link.addEventListener("click", function(event){
+var storage = localStorage.getItem("name");
+
+link.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  shadow.classList.add("modal-show");
+
+  if (storage) {
+    user.value = storage;
+    mail.focus();
+  } else {
+    user.focus();
+  }
+});
+
+close.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  shadow.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(event){
+  if (!user.value || !mail.value || !text.value){
     event.preventDefault();
-    popup.classList.add("modal-content-show");
-    shadow.classList.add('modal-show');
-    });
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("user", user.value)
+  }
+});
 
-  close.addEventListener("click", function(event){
-    event.preventDefault();
-    popup.classList.remove("modal-content-show");
-    shadow.classList.remove('modal-show');
-    });
-
-  form.addEventListener("submit", function(event){
-    if (!login.value || !mail.value){
-    event.preventDefault();
-    console.log("Нужно ввести данные");
-      }
-    });
+window.addEventListener("keydown", function(event){
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+      popup.classList.remove("modal-error");
+      shadow.classList.remove("modal-show");
+    }
+  }
+});
